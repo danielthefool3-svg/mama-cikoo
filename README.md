1e8debb19b4e631356b34f1a81cffb30fa321a96
Danil Muzakki <danielthefool3@gmail.com>
2026-01-12 01:33:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# web-valentine-card
+all this is for learning
+
+demo https://andresyaputra.github.io/web-valentine-card/
---
+++ star_game/Music-pages/js/music-list.js
@@ -0,0 +1,21 @@
+
+let allMusic = [
+  {
+    name: "Sweetest Smile ",
+    artist: "Mama Ciko",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "The 1975 - About You",
+    artist: "The 1975",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Cause You Have To",
+    artist: "LANY",
+    img: "music-3",
+    src: "music-3"
+  },
+];
